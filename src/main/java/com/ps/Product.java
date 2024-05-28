f27c2f4556b6c78f6e1d42e123601f18fb1c9c5a
Jeremy <ogprago@gmail.com>
2024-05-28 08:55:12 -0400

    Added properties to Sandwich with constructor, geters and seters. Made Product abstract and extended to Sandwich

--- src/main/java/com/ps/Product.java
+++ src/main/java/com/ps/Product.java
@@ -1,4 +1,22 @@
 package com.ps;
 
-public class Product {
+public abstract class Product {
+
+    private float price;
+
+    public Product(float price) {
+        this.price = price;
+    }
+
+    public abstract String getName();
+
+    public abstract float calcPrice();
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/ps/Sandwich.java
+++ src/main/java/com/ps/Sandwich.java
@@ -1,4 +1,109 @@
 package com.ps;
 
-public class Sandwich {
+public class Sandwich extends Product {
+
+    private String breadType;
+    private byte breadSize;
+    private String meatType;
+    private String cheeseType;
+    private String toppings;
+    private String sauces;
+    private boolean extra;
+    private boolean isToasted;
+
+    public Sandwich(
+            float price,
+            String breadType,
+            byte breadSize,
+            String meatType,
+            String cheeseType,
+            String toppings,
+            String sauces,
+            boolean extra,
+            boolean isToasted
+    ) {
+        super(price);
+        this.breadType = breadType;
+        this.breadSize = breadSize;
+        this.meatType = meatType;
+        this.cheeseType = cheeseType;
+        this.toppings = toppings;
+        this.sauces = sauces;
+        this.extra = extra;
+        this.isToasted = isToasted;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public float calcPrice() {
+        return 0;
+    }
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public byte getBreadSize() {
+        return breadSize;
+    }
+
+    public void setBreadSize(byte breadSize) {
+        this.breadSize = breadSize;
+    }
+
+    public String getMeatType() {
+        return meatType;
+    }
+
+    public void setMeatType(String meatType) {
+        this.meatType = meatType;
+    }
+
+    public String getCheeseType() {
+        return cheeseType;
+    }
+
+    public void setCheeseType(String cheeseType) {
+        this.cheeseType = cheeseType;
+    }
+
+    public String getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String toppings) {
+        this.toppings = toppings;
+    }
+
+    public String getSauces() {
+        return sauces;
+    }
+
+    public void setSauces(String sauces) {
+        this.sauces = sauces;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public void setIsToasted(boolean toasted) {
+        isToasted = toasted;
+    }
 }
